04828d997224a682f5ea3885c9c48c4fc5838fb2
Guest Account <Guest@blacki-li-rudi-migliozzis-iMac.local>
2011-06-06 15:24:40 -0400

    creating app and repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetUp::Application.config.secret_token = 'f55f00a31692d927b9e9b49084f0a7c06a1ed971c9613018356250d219da2bb0b55577125bfb015965a85a7270f071c17731fe7f7300820edad97cd68d12be71'
